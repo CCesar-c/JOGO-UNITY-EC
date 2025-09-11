17ac0add00626f231f663058dc9069096afbac27
CESAR_C# <lianbastiam@gmail.com>
2025-09-11 15:56:24 -0300

    fixed

--- Assets/script/moviment.cs
+++ Assets/script/moviment.cs
@@ -3,9 +3,24 @@ using UnityEngine;
 public class moviment : MonoBehaviour
 {
     public Rigidbody rb;
-    public float force  = 1000;
+    public Transform cam;
+    public float sensi = 200;
+    public float force = 1000;
+    float rotationX = 0f;
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X") * sensi * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensi * Time.deltaTime;
+
+        // --- Rotación vertical (solo cámara)
+        rotationX -= mouseY;
+
+        rotationX = Mathf.Clamp(rotationX, -80f, 80f);
+        cam.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+
+        // --- Rotación horizontal (solo jugador)
+        transform.Rotate(Vector3.up * mouseX);
+
         if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(force * Time.deltaTime * transform.forward);
