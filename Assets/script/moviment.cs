4930e318d1cc2daed8b35fed5490bfc1a7069f58
CESAR_C# <lianbastiam@gmail.com>
2025-09-11 17:13:57 -0300

    fixed

--- Assets/script/moviment.cs
+++ Assets/script/moviment.cs
@@ -1,14 +1,22 @@
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class moviment : MonoBehaviour
 {
     public Rigidbody rb;
     public Transform cam;
+    public GameObject arma;
     public float sensi = 200;
     public float force = 1000;
     float rotationX = 0f;
     void Update()
     {
+        Vector3 vara = new(0.5f, -0.5f, 1f);
+        Vector3 varb = new(-0.04f, -1.27f, 0.09f);
+        Quaternion q = new(0, 270, 30, 0);
+
         float mouseX = Input.GetAxis("Mouse X") * sensi * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensi * Time.deltaTime;
 
@@ -20,6 +28,17 @@ public class moviment : MonoBehaviour
 
         // --- Rotación horizontal (solo jugador)
         transform.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            arma.transform.SetLocalPositionAndRotation(Vector3.Lerp(vara, varb, 1f * Time.deltaTime), Quaternion.Lerp(arma.transform.localRotation, q, 1f * Time.deltaTime));
+            arma.transform.localScale = Vector3.Lerp(arma.transform.localScale, arma.transform.localScale = new Vector3(1, 5, 10), 1f * Time.deltaTime);
+        }
+        else
+        {
+            arma.transform.SetLocalPositionAndRotation(Vector3.Lerp(varb, vara, 1f * Time.deltaTime), Quaternion.Lerp(arma.transform.localRotation, new Quaternion(0,270,-10,0), 1f * Time.deltaTime));
+            arma.transform.localScale = Vector3.Lerp(arma.transform.localScale, arma.transform.localScale = new Vector3(1, 1, 1), 1f * Time.deltaTime);
+        }
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -42,7 +61,7 @@ public class moviment : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.Space))
         {
-            
+
         }
     }
 }
