14fae99f285830c2598eb2f8466bc8ba09b3dd5b
CCesar-c <lianbastiam@gmail.com>
2025-09-12 15:46:48 -0300

    fixed

--- Assets/script/moviment.cs
+++ Assets/script/moviment.cs
@@ -1,67 +1,60 @@
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class moviment : MonoBehaviour
 {
     public Rigidbody rb;
     public Transform cam;
     public GameObject arma;
-    public float sensi = 200;
-    public float force = 1000;
-    float rotationX = 0f;
+
+    public float sensi = 200f;
+    public float force = 1000f;
+    public float armaMoveSpeed = 5f; // Velocidad de transición del arma
+
+    private float rotationX = 0f;
+
+    // Posiciones y rotaciones objetivo del arma
+    private Vector3 armaPosAim = new Vector3(-0.02f, -1.29f, -0.12f);
+    private Quaternion armaRotAim = Quaternion.Euler(0, 270f, -30f);
+    private Vector3 armaScaleAim = new Vector3(1f, 5f, 10f);
+
+    private Vector3 armaPosHip = new Vector3(0.5f, -0.5f, 1.1f);
+    private Quaternion armaRotHip = Quaternion.Euler(0f, 270f, -10f);
+    private Vector3 armaScaleHip = new Vector3(1f, 1f, 1f);
+
     void Update()
     {
-        Vector3 vara = new(0.5f, -0.5f, 1f);
-        Vector3 varb = new(-0.04f, -1.27f, 0.09f);
-        Quaternion q = new(0, 270, 30, 0);
-
+        // --- Rotación de cámara y jugador
         float mouseX = Input.GetAxis("Mouse X") * sensi * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensi * Time.deltaTime;
 
-        // --- Rotación vertical (solo cámara)
         rotationX -= mouseY;
-
         rotationX = Mathf.Clamp(rotationX, -80f, 80f);
-        cam.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
 
-        // --- Rotación horizontal (solo jugador)
+        cam.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
 
+        // --- Movimiento del arma
         if (Input.GetKey(KeyCode.Mouse1))
         {
-            arma.transform.SetLocalPositionAndRotation(Vector3.Lerp(vara, varb, 1f * Time.deltaTime), Quaternion.Lerp(arma.transform.localRotation, q, 1f * Time.deltaTime));
-            arma.transform.localScale = Vector3.Lerp(arma.transform.localScale, arma.transform.localScale = new Vector3(1, 5, 10), 1f * Time.deltaTime);
+            arma.transform.localPosition = Vector3.Lerp(arma.transform.localPosition, armaPosAim, armaMoveSpeed * Time.deltaTime);
+            arma.transform.localRotation = Quaternion.Lerp(arma.transform.localRotation, armaRotAim, armaMoveSpeed * Time.deltaTime);
+            arma.transform.localScale = Vector3.Lerp(arma.transform.localScale, armaScaleAim, armaMoveSpeed * Time.deltaTime);
         }
         else
         {
-            arma.transform.SetLocalPositionAndRotation(Vector3.Lerp(varb, vara, 1f * Time.deltaTime), Quaternion.Lerp(arma.transform.localRotation, new Quaternion(0,270,-10,0), 1f * Time.deltaTime));
-            arma.transform.localScale = Vector3.Lerp(arma.transform.localScale, arma.transform.localScale = new Vector3(1, 1, 1), 1f * Time.deltaTime);
+            arma.transform.localPosition = Vector3.Lerp(arma.transform.localPosition, armaPosHip, armaMoveSpeed * Time.deltaTime);
+            arma.transform.localRotation = Quaternion.Lerp(arma.transform.localRotation, armaRotHip, armaMoveSpeed * Time.deltaTime);
+            arma.transform.localScale = Vector3.Lerp(arma.transform.localScale, armaScaleHip, armaMoveSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.AddForce(force * Time.deltaTime * transform.forward);
-        }
+        // --- Movimiento del jugador con Rigidbody
+        Vector3 moveDir = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(force * Time.deltaTime * -transform.forward);
-        }
+        if (Input.GetKey(KeyCode.W)) moveDir += transform.forward;
+        if (Input.GetKey(KeyCode.S)) moveDir -= transform.forward;
+        if (Input.GetKey(KeyCode.A)) moveDir -= transform.right;
+        if (Input.GetKey(KeyCode.D)) moveDir += transform.right;
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(force * Time.deltaTime * -transform.right);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(force * Time.deltaTime * transform.right);
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-
-        }
+        rb.AddForce(moveDir.normalized * force * Time.deltaTime, ForceMode.Force);
     }
 }
